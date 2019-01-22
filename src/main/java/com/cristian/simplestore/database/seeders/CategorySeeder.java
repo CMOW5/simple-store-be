0df87bcb2c7dbd377e53c252084a4003ee63a0dd
CMOW5 <tec.cmos@gmail.com>
2019-01-22 00:36:48 -0500

    a categories seeder to feed the database with initial test data

---
+++ src/main/java/com/cristian/simplestore/database/seeders/CategorySeeder.java
@@ -0,0 +1,79 @@
+package com.cristian.simplestore.database.seeders;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
+import com.cristian.simplestore.category.Category;
+import com.cristian.simplestore.category.CategoryRepository;
+import com.cristian.simplestore.image.Image;
+import com.cristian.simplestore.image.ImageRepository;
+import com.github.javafaker.Faker;
+
+@Service
+public class CategorySeeder {
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private ImageRepository imageRepository;
+	
+	private Faker faker;
+	
+	public static final long MAX_CATEGORIES = 10; 
+	
+	public CategorySeeder() {
+		faker = new Faker();
+	}
+	
+	public void seed() {
+		for (int i = 0; i < MAX_CATEGORIES; i++) {
+			try {
+				Category category = createRandomCategoryOnDB();
+				this.createRandomCategoriesWithParentOnDB(category, 4);
+			} catch (DataIntegrityViolationException e) {
+				continue;
+			}
+			
+		}
+	}
+	
+	private Category createRandomCategoryOnDB() {
+		Category category = new Category();
+		category.setName(faker.commerce().department());
+		category.addImage(generateRandomImageOnDB());
+		categoryRepository.save(category);
+		return category;
+	}
+	
+	private void createRandomCategoriesWithParentOnDB(Category parentCategory, long categoriesSize) {
+		for (int i = 0; i < categoriesSize; i++) {
+			Category category1 = new Category();
+			category1.setName(this.faker.commerce().department());
+			category1.setParentCategory(parentCategory);
+			category1.addImage(generateRandomImageOnDB());
+			categoryRepository.save(category1);
+			
+			Category category2 = new Category();
+			category2.setName(this.faker.commerce().department());
+			category2.setParentCategory(category1);
+			category2.addImage(generateRandomImageOnDB());
+			categoryRepository.save(category2);
+		}
+	}
+	
+	private Image generateRandomImageOnDB() {
+		String baseUrl = "https://picsum.photos/";
+		int width = this.faker.number().numberBetween(200, 800);
+		int height = this.faker.number().numberBetween(200, 800);
+		
+		String imageName = baseUrl + width + "/" + height;
+		
+		Image image = new Image();
+		image.setName(imageName);
+		imageRepository.save(image);
+		return image;
+	}
+	
+}
