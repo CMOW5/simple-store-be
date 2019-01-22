da206f5845aa4e182ca3ef73960fa35349e5c99e
CMOW5 <tec.cmos@gmail.com>
2019-01-22 00:31:12 -0500

    StorageService now inits with the app

--- src/main/java/com/cristian/simplestore/SimpleStoreApplication.java
+++ src/main/java/com/cristian/simplestore/SimpleStoreApplication.java
@@ -6,9 +6,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
+import com.cristian.simplestore.database.seeders.CategorySeeder;
+import com.cristian.simplestore.storage.ImageStorageService;
+import com.cristian.simplestore.storage.StorageProperties;
+import com.cristian.simplestore.storage.StorageService;
+
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class SimpleStoreApplication {
 	
 	private static final Logger log = LoggerFactory.getLogger(SimpleStoreApplication.class);
@@ -16,47 +23,14 @@ public class SimpleStoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleStoreApplication.class, args);
 	}
-	/*
+
 	@Bean
-	public CommandLineRunner demo(UserRepository repository) {
+	public CommandLineRunner demo(CategorySeeder categorySeeder, ImageStorageService storageService) {
 		return (args) -> {
-			// save a couple of customers
-			repository.save(new User("Jack", "Bauer"));
-			repository.save(new User("Chloe", "O'Brian"));
-			repository.save(new User("Kim", "Bauer"));
-			repository.save(new User("David", "Palmer"));
-			repository.save(new User("Michelle", "Dessler"));
-
-			// fetch all customers
-			log.info("Customers found with findAll():");
-			log.info("-------------------------------");
-			for (User customer : repository.findAll()) {
-				log.info(customer.toString());
-			}
-			log.info("");
-
-			// fetch an individual customer by ID
-			repository.findById(1L)
-				.ifPresent(customer -> {
-					log.info("Customer found with findById(1L):");
-					log.info("--------------------------------");
-					log.info(customer.toString());
-					log.info("");
-				});
-
-			// fetch customers by last name
-			log.info("Customer found with findByLastName('Bauer'):");
-			log.info("--------------------------------------------");
-			repository.findByLastName("Bauer").forEach(bauer -> {
-				log.info(bauer.toString());
-			});
-			// for (Customer bauer : repository.findByLastName("Bauer")) {
-			// 	log.info(bauer.toString());
-			// }
-			log.info("");
+			// categorySeeder.seed();
+			storageService.init();
 		};
 		
 	}
-	*/
 }
 
