6a829c52e0da698851ee2e2d01cd0e332c56c71b
bit5 <tec.cmos@gmail.com>
2019-02-26 18:04:52 -0500

    categories form to handle the form validation

---
+++ src/main/java/com/cristian/simplestore/form/CategoryCreateForm.java
@@ -0,0 +1,58 @@
+package com.cristian.simplestore.form;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.cristian.simplestore.category.Category;
+import com.cristian.simplestore.validators.Exists;
+import com.cristian.simplestore.validators.ExistsDb;
+
+@ExistsDb(
+  table = "categories",
+  columnName = "name",
+  columnValueField = "name"
+)
+public class CategoryCreateForm {
+	
+	@NotNull @Size(min = 2, max = 200) 
+	private String name;
+	
+	// Nullable and exists
+	private Category parentCategory;
+	
+	private MultipartFile image;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Category getParentCategory() {
+		return parentCategory;
+	}
+
+	public void setParentCategory(Category parentCategory) {
+		this.parentCategory = parentCategory;
+	}
+
+	public MultipartFile getImage() {
+		return image;
+	}
+
+	public void setImage(MultipartFile image) {
+		this.image = image;
+	}
+	
+	public Category getModel() {
+		Category category = new Category();
+		category.setName(name);
+		category.setParentCategory(parentCategory);
+		return category;
+	}
+	
+}
