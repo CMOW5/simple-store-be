4b75ed0a6601a58788b25c871e3f5585d943b40a
bit5 <tec.cmos@gmail.com>
2019-02-27 01:08:11 -0500

    unused imports deleted

--- src/main/java/com/cristian/simplestore/SimpleStoreApplication.java
+++ src/main/java/com/cristian/simplestore/SimpleStoreApplication.java
@@ -1,8 +1,5 @@
 package com.cristian.simplestore;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,16 +7,13 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 
 import com.cristian.simplestore.database.seeders.CategorySeeder;
-import com.cristian.simplestore.storage.ImageStorageService;
-import com.cristian.simplestore.storage.StorageProperties;
-import com.cristian.simplestore.storage.StorageService;
+import com.cristian.simplestore.services.ImageStorageService;
+import com.cristian.simplestore.storage.StorageConfig;
 
 @SpringBootApplication
-@EnableConfigurationProperties(StorageProperties.class)
+@EnableConfigurationProperties(StorageConfig.class)
 public class SimpleStoreApplication {
 	
-	private static final Logger log = LoggerFactory.getLogger(SimpleStoreApplication.class);
-
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleStoreApplication.class, args);
 	}
