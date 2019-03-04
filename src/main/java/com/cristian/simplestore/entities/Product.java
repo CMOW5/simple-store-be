4d87e7a6b09382d0281525dfaf9428f954681861
bit5 <tec.cmos@gmail.com>
2019-03-04 14:06:30 -0500

    One to many relationship with images added

--- src/main/java/com/cristian/simplestore/entities/Product.java
+++ src/main/java/com/cristian/simplestore/entities/Product.java
@@ -1,72 +1,81 @@
 package com.cristian.simplestore.entities;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.PositiveOrZero;
-import javax.validation.constraints.Size;
+
 
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-
+import org.springframework.web.multipart.MultipartFile;
 
 @Entity
 @Table(name = "products")
 public class Product {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private long id;
-	
+	private Long id;
+
 	@Column(unique = true, nullable = false)
-	@Size(min = 1, max = 100) @NotNull
 	private String name;
-	
-	@Size(min = 2, max = 200)
+
 	private String description;
 
 	@Column(nullable = false)
 	private double price;
-	
-	@PositiveOrZero
+
 	private double priceSale;
-	
+
 	@ColumnDefault("false")
 	private boolean inSale;
-	
+
 	@ColumnDefault("true")
 	private boolean active;
-	
+
 	@ManyToOne
 	private Category category;
+
+	@OneToMany(
+			mappedBy = "product", 
+			cascade = CascadeType.ALL, 
+			orphanRemoval = true
+	)
+	private List<ProductImage> images = new ArrayList<>();
 	
+	private Long units;
+
 	@CreationTimestamp
 	private LocalDateTime createdDate;
-	
+
 	@UpdateTimestamp
 	private Date updatedDate;
-	
-	public Product() {}
-	
+
+	public Product() {
+	}
+
 	public Product(String name, double price) {
 		this.name = name;
 		this.price = price;
 	}
 
-	public long getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
@@ -126,6 +135,51 @@ public class Product {
 		this.category = category;
 	}
 	
-	
+	public Long getUnits() {
+		return units;
+	}
 
+	public void setUnits(Long units) {
+		this.units = units;
+	}
+	
+	public List<Image> getImages() {
+		List<Image> images = new ArrayList<Image>();
+		List<ProductImage> productImages = this.images;
+		productImages.forEach(image -> images.add(image.getImage()));
+		return images;
+	}
+
+	public void addImage(Image image) {
+		ProductImage productImage = new ProductImage(this, image);
+		images.add(productImage);
+		image.getOwners().add(productImage);
+	}
+	
+	public void addImages(List<Image> images) {
+		for (Image image : images) {
+			addImage(image);
+		}
+	}
+
+	public void removeImage(Image image) {
+		ProductImage productImage = new ProductImage(this, image);
+		image.getOwners().remove(productImage);
+		images.remove(productImage);
+		productImage.setProduct(null);
+		productImage.setImage(null);
+	}
+	
+	public void removeImages(Iterable<Image> images) {
+		for (Image image: images) {
+			this.removeImage(image);
+		}
+	}
+	
+	public void removeImages(List<Image> images) {
+		for (Image image: images) {
+			this.removeImage(image);
+		}
+	}
+	
 }
