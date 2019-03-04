52e473ad4f6b9c4ffe71b2e94da5b038178c4bbb
bit5 <tec.cmos@gmail.com>
2019-03-04 14:04:31 -0500

    testcontroller

--- src/main/java/com/cristian/simplestore/controllers/TestController.java
+++ src/main/java/com/cristian/simplestore/controllers/TestController.java
@@ -1,9 +1,14 @@
 package com.cristian.simplestore.controllers;
 
+import java.util.List;
 import java.util.Map;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,8 +17,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.cristian.simplestore.entities.Image;
+import com.cristian.simplestore.entities.Product;
 import com.cristian.simplestore.forms.CategoryCreateForm;
 import com.cristian.simplestore.forms.CategoryUpdateForm;
+import com.cristian.simplestore.forms.ProductCreateForm;
+import com.cristian.simplestore.services.ImageService;
 import com.cristian.simplestore.utils.response.CustomResponse;
 ;
 
@@ -23,22 +32,35 @@ public class TestController {
 
 	private CustomResponse response = new CustomResponse();
 
-	// BindException
+	@Autowired ImageService imageService;
+	
+	@PersistenceContext EntityManager entityManager;
 
 	@RequestMapping(value = "/test", method = RequestMethod.POST)
+	// @Transactional
 	public Map<String, Object> create(
 			// @PathVariable Long id, 
-			@Valid CategoryUpdateForm form, 
-			MultipartFile image,
-			// HttpServletRequest request,
-			BindingResult validationResult) {
-
+			@Valid ProductCreateForm form 
+			// BindingResult validationResult
+			) {
+		
+		List<MultipartFile> imageFiles = form.getImages();
+	
+		Product product = form.getModel();
+		
+		entityManager.persist(product);
+		
+		
+		for (MultipartFile imageFile: imageFiles) {
+			Image image = new Image();
+			entityManager.persist(image);
+			product.addImage(image);
+		}
+			
+		entityManager.flush();
+		
 		String some = "some";
 
-		if (validationResult.hasErrors()) {
-			some = "dsds";
-		}
-		// Category category = categoryForm.getModel();
 
 		response.attachContent(some);
 		return response.build();
