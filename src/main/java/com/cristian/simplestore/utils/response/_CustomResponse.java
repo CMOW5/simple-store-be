00db443b77bb152e3c14ea4eb63eee9024989371
bit5 <tec.cmos@gmail.com>
2019-03-14 03:35:25 -0500

    class marked as deprecated

--- src/main/java/com/cristian/simplestore/utils/response/_CustomResponse.java
+++ src/main/java/com/cristian/simplestore/utils/response/_CustomResponse.java
@@ -3,23 +3,24 @@ package com.cristian.simplestore.utils.response;
 import java.util.HashMap;
 import java.util.Map;
 
-public class CustomResponse {
+@Deprecated
+public class _CustomResponse {
 	
 	private static final String CONTENT_KEY = "content";
 	
 	private Map<String, Object> attachments;
 	
-	public CustomResponse() {
+	public _CustomResponse() {
 		attachments = new HashMap<String, Object>();
 		this.attachments.put(CONTENT_KEY, null);
 	}
 	
-	public CustomResponse attachContent(Object content) {
+	public _CustomResponse attachContent(Object content) {
 		this.attachments.put(CONTENT_KEY, content);
 		return this;
 	}
 	
-	public CustomResponse attach(String key, Object value) {
+	public _CustomResponse attach(String key, Object value) {
 		this.attachments.put(key, value);
 		return this;
 	}
