968ad84d1a9118077e4e644db49ac28f666b76b6
bit5 <tec.cmos@gmail.com>
2019-03-18 01:07:03 -0500

    ExceptionHandler added to catch EntityNotFoundException

--- src/main/java/com/cristian/simplestore/controllers/CategoryController.java
+++ src/main/java/com/cristian/simplestore/controllers/CategoryController.java
@@ -11,12 +11,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cristian.simplestore.entities.Category;
@@ -38,7 +38,6 @@ public class CategoryController {
 	@GetMapping
 	public ResponseEntity<Map<String, Object>> findAllCategories() {
 		List<Category> categories = categoryService.findAllCategories();
-		
 		return response.status(HttpStatus.OK)
 					.content(categories)
 					.build();
@@ -46,23 +45,16 @@ public class CategoryController {
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Map<String, Object>> findCategoryById(@PathVariable long id) {
-		try {
-			Category foundCategory = categoryService.findCategoryById(id);
-			return response.status(HttpStatus.OK)
-					.content(foundCategory)
-					.build();
-		} catch (EntityNotFoundException exception) {
-			return response.status(HttpStatus.NOT_FOUND)
-					.content(null)
-					.build();
-		}
+		Category foundCategory = categoryService.findCategoryById(id);
+		return response.status(HttpStatus.OK)
+				.content(foundCategory)
+				.build();
 	}
 	
 	@PostMapping
 	public ResponseEntity<Map<String, Object>> create(
 			@Valid CategoryCreateForm form) {
 		Category createdCategory = categoryService.create(form);
-		
 		return response.status(HttpStatus.CREATED)
 					.content(createdCategory)
 					.build();
@@ -71,41 +63,35 @@ public class CategoryController {
 	@PutMapping("/{id}")
 	public ResponseEntity<Map<String, Object>> update(
 			@Valid CategoryUpdateForm form) {
-		try {
-			Category updatedCategory = categoryService.update(form);
-			return response.status(HttpStatus.OK)
-					.content(updatedCategory)
-					.build();
-		} catch (EntityNotFoundException exception) {
-			return response.status(HttpStatus.NOT_FOUND)
-					.content(null)
-					.build();
-		}
+		Category updatedCategory = categoryService.update(form);
+		return response.status(HttpStatus.OK)
+				.content(updatedCategory)
+				.build();
 	}
 	
 	@DeleteMapping("/{id}")
-	@ResponseStatus(code=HttpStatus.NO_CONTENT)
 	public ResponseEntity<Map<String, Object>> delete(@PathVariable Long id) {
-		try {
-			categoryService.deleteById(id);
-			return response.status(HttpStatus.NO_CONTENT)
-					.content(null)
-					.build();
-		} catch (EntityNotFoundException exception) {
-			return response.status(HttpStatus.NOT_FOUND)
-					.content(null)
-					.build();
-		}
+		categoryService.deleteById(id);
+		return response.status(HttpStatus.NO_CONTENT)
+				.content(null)
+				.build();
 	}
 	
 	@GetMapping("/count")
 	public ResponseEntity<Map<String, Object>> count() {
 		long categoriesCount = categoryService.count();
-		
 		return response.status(HttpStatus.OK)
 				.content(categoriesCount)
 				.build();	
 	}
+	
+	@ExceptionHandler(EntityNotFoundException.class)
+	public ResponseEntity<Map<String, Object>> handleError(Exception ex) {
+		return response.status(HttpStatus.NOT_FOUND)
+				.content(null)
+				.build();
+	}
+	
 	
 	// org.springframework.validation.BindException
 	
--- src/main/java/com/cristian/simplestore/controllers/ProductController.java
+++ src/main/java/com/cristian/simplestore/controllers/ProductController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,16 +45,10 @@ public class ProductController {
 
 	@GetMapping("/{id}")
 	public ResponseEntity<Map<String, Object>> findProductById(@PathVariable long id) {
-		try {
-			Product product = productService.findById(id);
-			return response.status(HttpStatus.OK)
-					.content(product)
-					.build();
-		} catch (EntityNotFoundException exception) {
-			return response.status(HttpStatus.NOT_FOUND)
-					.content(null)
-					.build();
-		} 
+		Product product = productService.findById(id);
+		return response.status(HttpStatus.OK)
+				.content(product)
+				.build();
 	}
 	
 	@PostMapping
@@ -67,31 +62,19 @@ public class ProductController {
 	@PutMapping("/{id}")
 	public ResponseEntity<Map<String, Object>> update(@PathVariable long id, 
 			@Valid ProductUpdateForm form) {
-		try {
-			Product updatedProduct = productService.update(form);
-			return response.status(HttpStatus.OK)
-					.content(updatedProduct)
-					.build();
-		} catch (EntityNotFoundException exception) {
-			return response.status(HttpStatus.NOT_FOUND)
-					.content(null)
-					.build();
-		}
+		Product updatedProduct = productService.update(form);
+		return response.status(HttpStatus.OK)
+				.content(updatedProduct)
+				.build();
 		
 	}
 
 	@DeleteMapping("/{id}")
 	public ResponseEntity<Map<String, Object>> delete(@PathVariable long id) {
-		try {
-			productService.deleteById(id);
-			return response.status(HttpStatus.NO_CONTENT)
-					.content(null)
-					.build();
-		} catch (EntityNotFoundException exception) {
-			return response.status(HttpStatus.NOT_FOUND)
-					.content(null)
-					.build();
-		}
+		productService.deleteById(id);
+		return response.status(HttpStatus.NO_CONTENT)
+				.content(null)
+				.build();
 	}
 
 	@GetMapping("/count")
@@ -99,6 +82,13 @@ public class ProductController {
 		long productsCount = productService.count();
 		return response.status(HttpStatus.OK)
 				.content(productsCount)
+				.build();
+	}
+	
+	@ExceptionHandler(EntityNotFoundException.class)
+	public ResponseEntity<Map<String, Object>> handleError(Exception ex) {
+		return response.status(HttpStatus.NOT_FOUND)
+				.content(null)
 				.build();
 	}
 }
