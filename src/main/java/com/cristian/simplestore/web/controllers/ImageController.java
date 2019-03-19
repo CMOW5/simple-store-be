4b84fc53567546eaebc8416f2831afadc22675c3
bit5 <tec.cmos@gmail.com>
2019-03-19 03:29:57 -0500

    produces mediatype header deleted

--- src/main/java/com/cristian/simplestore/web/controllers/ImageController.java
+++ src/main/java/com/cristian/simplestore/web/controllers/ImageController.java
@@ -6,7 +6,6 @@ import java.io.InputStream;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,8 +20,7 @@ public class ImageController {
 	@Autowired ImageStorageService imageStorageService;
 	
 	@GetMapping(
-			value = "api/images/{filename:..+}",
-			produces = MediaType.IMAGE_JPEG_VALUE
+			value = "api/images/{filename:..+}"
 	)
 	public @ResponseBody byte[] serveImage(@PathVariable String filename) throws IOException {
 		Resource file;
