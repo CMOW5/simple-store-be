b255dbdcf34f81f2c8f6aff21db6fd948984fee8
bit5 <tec.cmos@gmail.com>
2019-03-22 11:10:57 -0500

    fixes 15

---
+++ src/main/java/com/cristian/simplestore/web/controllers/interceptors/MaxFileUploadInterceptor.java
@@ -0,0 +1,29 @@
+package com.cristian.simplestore.web.controllers.interceptors;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+
+import com.cristian.simplestore.web.utils.response.ApiError;
+import com.cristian.simplestore.web.utils.response.ApiResponse;
+
+@ControllerAdvice
+@ResponseBody
+public class MaxFileUploadInterceptor {
+	
+	@ExceptionHandler(MaxUploadSizeExceededException.class)
+	public ResponseEntity<?> handleMaxUploadSize(
+			HttpServletRequest request,
+			MaxUploadSizeExceededException exception) {
+		ApiError error = new ApiError("file", exception.getMessage(), null);
+		return new ApiResponse()
+				.addError(error)
+				.status(HttpStatus.BAD_REQUEST)
+				.build();
+	}
+}
