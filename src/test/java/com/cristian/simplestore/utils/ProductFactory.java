3723f4c0257657e4b04f437320d4a2437acbeaa2
bit5 <tec.cmos@gmail.com>
2019-03-27 00:17:53 -0500

    clean up

--- src/test/java/com/cristian/simplestore/utils/ProductFactory.java
+++ src/test/java/com/cristian/simplestore/utils/ProductFactory.java
@@ -1,12 +1,8 @@
 package com.cristian.simplestore.utils;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.web.multipart.MultipartFile;
 
-import com.cristian.simplestore.persistence.entities.Category;
 import com.cristian.simplestore.persistence.entities.Product;
 import com.cristian.simplestore.web.forms.ProductCreateForm;
 import com.cristian.simplestore.web.forms.ProductUpdateForm;
@@ -17,6 +13,10 @@ public class ProductFactory {
 	
 	private Faker faker = new Faker();	
 	private static double MAX_PRICE = 10000000000.0;
+	private static int MAX_STOCK = 100000000;
+	
+	@Autowired
+	CategoryTestsUtils categoryUtils;
 	
 	@Autowired
 	ImageTestsUtils imageUtils;
@@ -24,23 +24,14 @@ public class ProductFactory {
 	public Product generateRandomProduct() {
 		Product product = new Product();
 		
-		String name = faker.commerce().productName();
-		String description = faker.commerce().productName();
-		String price = faker.commerce().price(0, MAX_PRICE); 
-		String priceSale = faker.commerce().price(0, MAX_PRICE);
-		boolean inSale = false;
-		boolean active = true;
-		Category category = null;
-		Long stock = (long) faker.number().numberBetween(0, 200);
-
-		product.setName(name);
-		product.setDescription(description);
-		product.setPrice(Double.valueOf(price));
-		product.setPriceSale(Double.valueOf(priceSale));
-		product.setInSale(inSale);
-		product.setActive(active);
-		product.setCategory(category);
-		product.setStock(stock);
+		product.setName(generateRandomName());
+		product.setDescription(generateRandomDescription());
+		product.setPrice(generateRandomPrice());
+		product.setPriceSale(generateRandomPrice());
+		product.setInSale(false);
+		product.setActive(true);
+		product.setStock(generateRandomStock());
+		product.setCategory(categoryUtils.saveRandomCategoryOnDB());
 		
 		return product;
 	}
@@ -48,52 +39,48 @@ public class ProductFactory {
 	public ProductCreateForm generateRandomProductCreateForm() {
 		ProductCreateForm form = new ProductCreateForm();
 		
-		String name = faker.commerce().productName();
-		String description = faker.lorem().sentence();
-		Double price = Double.valueOf(faker.commerce().price(0, Double.MAX_VALUE)); 
-		Double priceSale = Double.valueOf(faker.commerce().price(0, Double.MAX_VALUE)); 
-		boolean inSale = false;
-		boolean active = true;
-		Category category = null;
-		Long stock = (long) faker.number().numberBetween(0, 200);
-		List<MultipartFile> images = imageUtils.generateRandomImageFiles(2);
-		
-		form.setName(name);
-		form.setDescription(description);
-		form.setPrice(price);
-		form.setPriceSale(priceSale);
-		form.setInSale(inSale);
-		form.setActive(active);
-		form.setCategory(category);
-		form.setStock(stock);
-		form.setImages(images);
-		
+		form.setName(generateRandomName());
+		form.setDescription(generateRandomDescription());
+		form.setPrice(generateRandomPrice());
+		form.setPriceSale(generateRandomPrice());
+		form.setInSale(false);
+		form.setActive(true);
+		form.setStock(generateRandomStock());
+		form.setCategory(categoryUtils.saveRandomCategoryOnDB());
+		form.setImages(imageUtils.generateRandomMultiPartFiles(2));
+	
 		return form;
 	}
 	
 	public ProductUpdateForm generateRandomProductUpdateForm() {
 		ProductUpdateForm form = new ProductUpdateForm();
-		
-		String name = faker.commerce().productName();
-		String description = faker.lorem().sentence();
-		Double price = Double.valueOf(faker.commerce().price(0, Double.MAX_VALUE)); 
-		Double priceSale = Double.valueOf(faker.commerce().price(0, Double.MAX_VALUE)); 
-		boolean inSale = false;
-		boolean active = true;
-		Category category = null;
-		Long stock = (long) faker.number().numberBetween(0, 200);
-		List<MultipartFile> newImages = imageUtils.generateRandomImageFiles(2);
-		
-		form.setName(name);
-		form.setDescription(description);
-		form.setPrice(price);
-		form.setPriceSale(priceSale);
-		form.setInSale(inSale);
-		form.setActive(active);
-		form.setCategory(category);
-		form.setStock(stock);
-		form.setNewImages(newImages);
+				
+		form.setName(generateRandomName());
+		form.setDescription(generateRandomDescription());
+		form.setPrice(generateRandomPrice());
+		form.setPriceSale(generateRandomPrice());
+		form.setInSale(false);
+		form.setActive(true);
+		form.setStock(generateRandomStock());
+		form.setCategory(categoryUtils.saveRandomCategoryOnDB());
+		form.setNewImages(imageUtils.generateRandomMultiPartFiles(2));
 		
 		return form;
+	}
+	
+	private String generateRandomName() {
+		return faker.name().firstName();
+	}
+	
+	private String generateRandomDescription() {
+		return faker.lorem().sentence();
+	}
+	
+	private double generateRandomPrice() {
+		return Double.valueOf(faker.commerce().price(0, MAX_PRICE)); 
+	}
+	
+	private long generateRandomStock() {
+		return (long) faker.number().numberBetween(0, MAX_STOCK);
 	}
 }
