86ee7cba55b1985f5fac7f431564b7b21406b4a8
bit5 <tec.cmos@gmail.com>
2019-03-31 15:45:41 -0500

    update method refactored

--- src/main/java/com/cristian/simplestore/business/services/CategoryService.java
+++ src/main/java/com/cristian/simplestore/business/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 import javax.persistence.EntityNotFoundException;
+import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
@@ -42,6 +43,7 @@ public class CategoryService {
     return categoryRepository.save(category);
   }
 
+  @Transactional
   public Category create(CategoryCreateForm form) {
     Category category = new Category();
     category.setName(form.getName());
@@ -56,6 +58,7 @@ public class CategoryService {
     return categoryRepository.save(category);
   }
 
+  @Transactional
   public Category update(CategoryUpdateForm form) {
     try {
       Category storedCategory = categoryRepository.findById(form.getId()).get();
@@ -67,23 +70,21 @@ public class CategoryService {
       storedCategory.setName(newName);
       storedCategory = updateParentCategory(storedCategory, newParentCategory);
       storedCategory = updateCategoryImage(storedCategory, newImageFile, imageIdToDelete);
-      return categoryRepository.save(storedCategory);
+
+      return storedCategory;
     } catch (NoSuchElementException e) {
       throw new EntityNotFoundException("The category with the given id was not found");
     }
   }
 
   private Category updateParentCategory(Category categoryToUpdate, Category newParentCategory) {
-    // TODO: should i throw an exception here??
-    if (isTheSameCategory(categoryToUpdate, newParentCategory)) {
-      newParentCategory = null;
-    }
-
-    // here we avoid circular references between the category
-    // to update and its subcategories. for instance
-    // null -> A -> B -> C to C -> A -> B -> C is not allowed,
-    // the result will be: null -> C -> A -> B
-    if (categoryToUpdate.hasSubcategory(newParentCategory)) {
+    if (categoryToUpdate.equals(newParentCategory)) {
+      newParentCategory = categoryToUpdate.getParentCategory();
+    } else if (categoryToUpdate.hasSubcategory(newParentCategory)) {
+      // here we avoid circular references between the category
+      // to update and its subcategories. for instance
+      // null -> A -> B -> C to C -> A -> B -> C is not allowed,
+      // the result will be: null -> C -> A -> B
       newParentCategory.setParentCategory(categoryToUpdate.getParentCategory());
       categoryRepository.save(newParentCategory);
     }
@@ -99,15 +100,9 @@ public class CategoryService {
       Image newImage = imageService.save(newImageFile);
       Image currentImage = categoryToUpdate.getImage();
       categoryToUpdate.setImage(newImage);
-
-      // TODO: this step is necessary because the image has a constraint with the
-      // current category, so we need to update the category relationship with the
-      // current image first, and then we can safely delete the current image
-      categoryRepository.save(categoryToUpdate);
       imageService.delete(currentImage);
     } else if (imageIdToDelete != null) { // TODO: verify the imageId
       categoryToUpdate.deleteImage();
-      categoryRepository.save(categoryToUpdate);
       imageService.deleteById(imageIdToDelete);
     }
 
@@ -133,16 +128,5 @@ public class CategoryService {
       category.setImage(image);
     }
     return category;
-  }
-
-
-
-  // TODO: replace this with a hasCode and isEquals impl
-  private boolean isTheSameCategory(Category category1, Category category2) {
-    if ((category1 != null && category2 != null) && (category1.getId() == category2.getId())) {
-      return true;
-    } else {
-      return false;
-    }
   }
 }
