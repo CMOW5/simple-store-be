ed15798646cd800baf22a453149dbd442e5b86cd
bit5 <tec.cmos@gmail.com>
2019-04-02 21:52:26 -0500

    the parent category is now mapped with a map

--- src/main/java/com/cristian/simplestore/web/dto/CategoryResponseDto.java
+++ src/main/java/com/cristian/simplestore/web/dto/CategoryResponseDto.java
@@ -1,8 +1,11 @@
 package com.cristian.simplestore.web.dto;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Objects;
 import com.cristian.simplestore.persistence.entities.Category;
 import com.cristian.simplestore.persistence.entities.Image;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -14,9 +17,12 @@ public class CategoryResponseDto implements EntityDto<Category> {
 
   private String name;
 
-  private ParentCategoryResponseDto parentCategory;
+  private Map<String, Object> parentCategory;
 
   private ImageResponseDto image;
+
+  @JsonIgnore
+  private Category parentCategoryObject;
 
   public CategoryResponseDto(Category category) {
     id = category.getId();
@@ -25,17 +31,17 @@ public class CategoryResponseDto implements EntityDto<Category> {
     mapParentCategory(category.getParentCategory());
   }
 
-  private void mapImage(Image image) {
-    if (image != null) {
-      this.image = new ImageResponseDto(image);
+  private void mapParentCategory(Category parentCategory) {
+    if (parentCategory != null) {
+      this.parentCategory = new LinkedHashMap<String, Object>();
+      this.parentCategory.put("id", parentCategory.getId());
+      this.parentCategory.put("name", parentCategory.getName());
+      this.parentCategoryObject = parentCategory;
     }
   }
 
-  private void mapParentCategory(Category parentCategory) {
-    if (parentCategory != null) {
-      this.parentCategory =
-          new ParentCategoryResponseDto(parentCategory.getId(), parentCategory.getName());
-    }
+  private void mapImage(Image image) {
+    this.image = ImageResponseDto.build(image);
   }
 
   @Override
