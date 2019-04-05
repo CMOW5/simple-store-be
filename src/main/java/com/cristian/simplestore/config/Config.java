2e8bfe01c45583e40f76ad2a52a9bb84ffba41cd
bit5 <tec.cmos@gmail.com>
2019-04-04 23:03:31 -0500

    oauth2 security added

--- src/main/java/com/cristian/simplestore/config/Config.java
+++
@@ -1,9 +0,0 @@
-package com.cristian.simplestore.config;
-
-import org.springframework.context.annotation.Configuration;
-
-
-@Configuration
-public class Config {
-
-}
--- src/main/java/com/cristian/simplestore/config/WebMvcConfig.java
+++ src/main/java/com/cristian/simplestore/config/WebMvcConfig.java
@@ -7,9 +7,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
+public class WebMvcConfig implements WebMvcConfigurer {
+
+  private final long MAX_AGE_SECS = 3600;
+
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
+    registry.addMapping("/**").allowedOrigins("*")
+        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS").allowedHeaders("*")
+        .allowCredentials(true).maxAge(MAX_AGE_SECS);
   }
 }
