f2f034d270127752683f1dc62ce8a2c3f998894b
bit5 <tec.cmos@gmail.com>
2019-04-04 23:05:18 -0500

    user table, entity and repository created

---
+++ src/main/java/com/cristian/simplestore/persistence/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.cristian.simplestore.persistence.repositories;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.cristian.simplestore.persistence.entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+  Optional<User> findByEmail(String email);
+
+  Boolean existsByEmail(String email);
+
+}
