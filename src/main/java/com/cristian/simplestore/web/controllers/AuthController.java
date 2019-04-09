14b903876b1f86fb079da3f5792f9c61b80947f8
bit5 <tec.cmos@gmail.com>
2019-04-09 00:38:28 -0500

    the services now handle the auth logic

--- src/main/java/com/cristian/simplestore/web/controllers/AuthController.java
+++ src/main/java/com/cristian/simplestore/web/controllers/AuthController.java
@@ -1,93 +1,42 @@
 package com.cristian.simplestore.web.controllers;
 
-import com.cristian.simplestore.security.oauth2.AuthProvider;
 import com.cristian.simplestore.web.dto.request.user.LoginRequest;
 import com.cristian.simplestore.web.dto.request.user.SignUpRequest;
-import com.cristian.simplestore.web.dto.response.AuthResponse;
-import com.cristian.simplestore.web.dto.response.user.UserResponseDto;
-import com.cristian.simplestore.web.exceptions.BadRequestException;
+import com.cristian.simplestore.web.dto.response.LoginResponse;
+import com.cristian.simplestore.web.dto.response.user.UserResponse;
 import com.cristian.simplestore.web.utils.response.ApiResponse;
+import com.cristian.simplestore.business.services.AuthService;
 import com.cristian.simplestore.persistence.entities.User;
-import com.cristian.simplestore.persistence.repositories.UserRepository;
 import com.cristian.simplestore.security.CurrentUser;
-import com.cristian.simplestore.security.TokenProvider;
 import com.cristian.simplestore.security.UserPrincipal;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
-import java.net.URI;
 
-@Profile("!test") // TODO: fix this: AuthenticationManager authenticationManager is not wired
-                  // correctly in test profile
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
-
+  
   @Autowired
-  private AuthenticationManager authenticationManager;
-
-  @Autowired
-  private UserRepository userRepository;
-
-  @Autowired
-  private PasswordEncoder passwordEncoder;
-
-  @Autowired
-  private TokenProvider tokenProvider;
+  private AuthService authService;
 
   @PostMapping("/login")
   public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
-
-    Authentication authentication =
-        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
-            loginRequest.getEmail(), loginRequest.getPassword()));
-
-    SecurityContextHolder.getContext().setAuthentication(authentication);
-
-    User user =
-        userRepository.findById(((UserPrincipal) authentication.getPrincipal()).getId()).get();
-
-    String token = tokenProvider.createToken(authentication);
-
-    return new ApiResponse().status(HttpStatus.OK).content(new AuthResponse(token, user))
-        .addHeader("Authorization", "Bearer " + token).build();
+    LoginResponse loginResponse = authService.login(loginRequest);
+    
+    return new ApiResponse().status(HttpStatus.OK).content(loginResponse)
+        .addHeader("Authorization", "Bearer " + loginResponse.getToken()).build();
   }
 
   @PostMapping("/signup")
   public ResponseEntity<?> registerUser(@Valid @RequestBody SignUpRequest signUpRequest) {
-    if (userRepository.existsByEmail(signUpRequest.getEmail())) {
-      throw new BadRequestException("Email address already in use.");
-    }
-
-    // Creating user's account
-    User user = new User();
-    user.setName(signUpRequest.getName());
-    user.setEmail(signUpRequest.getEmail());
-    user.setPassword(signUpRequest.getPassword());
-    user.setProvider(AuthProvider.local);
-
-    user.setPassword(passwordEncoder.encode(user.getPassword()));
-
-    User result = userRepository.save(user);
-
-    URI location = ServletUriComponentsBuilder.fromCurrentContextPath().path("/user/me")
-        .buildAndExpand(result.getId()).toUri();
-
-
-    return new ApiResponse().status(HttpStatus.CREATED).content("User registered successfully@")
+    User user = authService.signup(signUpRequest);
+    return new ApiResponse().status(HttpStatus.CREATED).content(UserResponse.build(user))
         .build();
-
-    // return ResponseEntity.created(location)
-    // .body(new ApiResponse(true, "User registered successfully@"));
   }
   
   @PostMapping("/logout")
@@ -95,6 +44,12 @@ public class AuthController {
    // TODO: ban the token
     return new ApiResponse().status(HttpStatus.OK).content("logged out").build();
   }
-
+  
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<?> handleControllersValidationMethodArgumentNotValidException(
+      HttpServletRequest request, MethodArgumentNotValidException exception) {
+    return new ApiResponse().errors(exception.getBindingResult().getFieldErrors())
+        .status(HttpStatus.BAD_REQUEST).build();
+  }
 
 }
