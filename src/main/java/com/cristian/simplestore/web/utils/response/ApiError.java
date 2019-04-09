fe337d3c456251ceec84c74d28a55483894a4f00
bit5 <tec.cmos@gmail.com>
2019-04-09 00:41:02 -0500

    lombok added

--- src/main/java/com/cristian/simplestore/web/utils/response/ApiError.java
+++ src/main/java/com/cristian/simplestore/web/utils/response/ApiError.java
@@ -1,14 +1,16 @@
 package com.cristian.simplestore.web.utils.response;
 
 import org.springframework.validation.FieldError;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
 public class ApiError {
 
   private String field;
   private String defaultMessage;
   private Object rejectedValue;
-
-  public ApiError() {}
 
   public ApiError(FieldError fieldError) {
     this.field = fieldError.getField();
@@ -21,29 +23,4 @@ public class ApiError {
     this.defaultMessage = defaultMessage;
     this.rejectedValue = rejectedValue;
   }
-
-  public String getField() {
-    return field;
-  }
-
-  public void setField(String field) {
-    this.field = field;
-  }
-
-  public String getDefaultMessage() {
-    return defaultMessage;
-  }
-
-  public void setDefaultMessage(String defaultMessage) {
-    this.defaultMessage = defaultMessage;
-  }
-
-  public Object getRejectedValue() {
-    return rejectedValue;
-  }
-
-  public void setRejectedValue(Object rejectedValue) {
-    this.rejectedValue = rejectedValue;
-  }
-
 }
