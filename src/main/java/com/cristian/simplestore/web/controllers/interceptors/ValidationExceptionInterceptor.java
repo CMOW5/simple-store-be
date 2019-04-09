204905572e92f0cfce78ea07e707f94d6e61f1f4
bit5 <tec.cmos@gmail.com>
2019-04-09 00:39:20 -0500

    added a MethodArgumentNotValidException handler

--- src/main/java/com/cristian/simplestore/web/controllers/interceptors/ValidationExceptionInterceptor.java
+++ src/main/java/com/cristian/simplestore/web/controllers/interceptors/ValidationExceptionInterceptor.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,6 +18,13 @@ public class ValidationExceptionInterceptor {
   public ResponseEntity<?> handleControllersValidationException(HttpServletRequest request,
       BindException exception) {
     return new ApiResponse().errors(exception).status(HttpStatus.BAD_REQUEST).build();
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<?> handleControllersValidationMethodArgumentNotValidException(
+      HttpServletRequest request, MethodArgumentNotValidException exception) {
+    return new ApiResponse().errors(exception.getBindingResult().getFieldErrors())
+        .status(HttpStatus.BAD_REQUEST).build();
   }
 
 
