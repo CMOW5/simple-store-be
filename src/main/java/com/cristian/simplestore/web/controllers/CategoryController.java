dceb0bead79ae25ac02c54af81534304c6fdd585
bit5 <tec.cmos@gmail.com>
2019-05-07 18:35:30 -0500

    pagination added

--- src/main/java/com/cristian/simplestore/web/controllers/CategoryController.java
+++ src/main/java/com/cristian/simplestore/web/controllers/CategoryController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,9 +13,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.cristian.simplestore.business.services.CategoryService;
 import com.cristian.simplestore.persistence.entities.Category;
+import com.cristian.simplestore.web.CustomPaginator;
+import com.cristian.simplestore.web.CustomPaginatorImp;
 import com.cristian.simplestore.web.dto.request.category.CategoryCreateRequest;
 import com.cristian.simplestore.web.dto.request.category.CategoryUpdateRequest;
 import com.cristian.simplestore.web.dto.response.CategoryResponse;
@@ -31,9 +35,11 @@ public class CategoryController {
   private CategoryService categoryService;
 
   @GetMapping
-  public ResponseEntity<?> findAllCategories() {
-    List<Category> categories = categoryService.findAll();
-    return response.status(HttpStatus.OK).content(convertEntitiesToDto(categories)).build();
+  public ResponseEntity<?> findAllCategories(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "20") int size) {
+    Page<Category> result = categoryService.findAll(page, size);
+    List<CategoryResponse> categoriesResponse = convertEntitiesToDto(result.getContent());
+    CustomPaginator paginator = new CustomPaginatorImp(result);
+    return response.status(HttpStatus.OK).content(categoriesResponse).paginator(paginator).build();
   }
 
   @GetMapping(value = "/{id}")
