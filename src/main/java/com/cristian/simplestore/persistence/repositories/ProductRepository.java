72aab76874e31d8a051b683717ad90223ee97ebc
bit5 <tec.cmos@gmail.com>
2019-05-07 18:31:48 -0500

    the class now extends JpaRepository

--- src/main/java/com/cristian/simplestore/persistence/repositories/ProductRepository.java
+++ src/main/java/com/cristian/simplestore/persistence/repositories/ProductRepository.java
@@ -1,7 +1,7 @@
 package com.cristian.simplestore.persistence.repositories;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import com.cristian.simplestore.persistence.entities.Product;
 
-public interface ProductRepository extends PagingAndSortingRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
