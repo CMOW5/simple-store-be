4ffb4b88a736bb11bcfbd3653d9e5487345241e8
bit5 <tec.cmos@gmail.com>
2019-05-07 18:36:46 -0500

    test controller

--- src/main/java/com/cristian/simplestore/web/controllers/TestController.java
+++ src/main/java/com/cristian/simplestore/web/controllers/TestController.java
@@ -1,15 +1,26 @@
 package com.cristian.simplestore.web.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import com.cristian.simplestore.business.services.CategoryService;
 import com.cristian.simplestore.business.services.ImageService;
+import com.cristian.simplestore.persistence.entities.Category;
+import com.cristian.simplestore.web.CustomPaginatorImp;
 import com.cristian.simplestore.web.dto.request.TestForm;
+import com.cristian.simplestore.web.dto.response.CategoryResponse;
 import com.cristian.simplestore.web.utils.response.ApiResponse;
 
 
@@ -23,11 +34,27 @@ public class TestController {
 
   @PersistenceContext
   EntityManager entityManager;
+  
+  @Autowired
+  private CategoryService categoryService;
 
-  @RequestMapping(value = "/test", method = RequestMethod.POST)
-  public ResponseEntity<?> create(@Valid TestForm form) {
-    String some = "some";
-    response.content(some);
-    return response.build();
+  @GetMapping("api/test")
+  public ResponseEntity<?> findAllCategories(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "20") int size) {
+    Page<Category> result = categoryService.findAll(page, size);
+    List<CategoryResponse> categoriesResponse = convertEntitiesToDto(result.getContent());
+    Pageable paginator = result.getPageable();
+    CustomPaginatorImp customPaginator = new CustomPaginatorImp(result);
+    return response.status(HttpStatus.OK).content(result.getTotalPages()).paginator(customPaginator).build();
+    // return response.status(HttpStatus.OK).content(categoriesResponse).paginator(paginator).build();
+  }
+  
+  private CategoryResponse convertEntityToDto(Category category) {
+    return new CategoryResponse(category);
+  }
+
+  private List<CategoryResponse> convertEntitiesToDto(List<Category> categories) {
+    List<CategoryResponse> dtos = new ArrayList<>();
+    categories.forEach(category -> dtos.add(convertEntityToDto(category)));
+    return dtos;
   }
 }
