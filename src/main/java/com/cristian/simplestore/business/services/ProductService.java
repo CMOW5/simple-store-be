cf46d4a7e8bf2c3a3ab3f18a455c54bc0534e0b3
bit5 <tec.cmos@gmail.com>
2019-05-08 14:36:56 -0500

    findAll(page, size) method added to enable pagination

--- src/main/java/com/cristian/simplestore/business/services/ProductService.java
+++ src/main/java/com/cristian/simplestore/business/services/ProductService.java
@@ -1,6 +1,7 @@
 package com.cristian.simplestore.business.services;
 
 import java.util.List;
+import org.springframework.data.domain.Page;
 import com.cristian.simplestore.persistence.entities.Product;
 import com.cristian.simplestore.web.dto.request.product.ProductCreateRequest;
 import com.cristian.simplestore.web.dto.request.product.ProductUpdateRequest;
@@ -8,6 +9,8 @@ import com.cristian.simplestore.web.dto.request.product.ProductUpdateRequest;
 public interface ProductService {
 
   List<Product> findAll();
+  
+  Page<Product> findAll(int page, int size);
 
   Product findById(Long id);
 
@@ -20,4 +23,6 @@ public interface ProductService {
   void deleteById(Long id);
 
   public long count();
+
+  
 }
--- src/main/java/com/cristian/simplestore/business/services/ProductServiceImpl.java
+++ src/main/java/com/cristian/simplestore/business/services/ProductServiceImpl.java
@@ -31,6 +31,11 @@ public class ProductServiceImpl implements ProductService {
     productRepository.findAll().forEach(products::add);
     return products;
   }
+  
+  @Override
+  public Page<Product> findAll(int page, int size) {
+    return productRepository.findAll(PageRequest.of(page, size));
+  }
 
   public Product findById(Long id) {
     return productRepository.findById(id).orElseThrow(
@@ -109,5 +114,4 @@ public class ProductServiceImpl implements ProductService {
   public long count() {
     return productRepository.count();
   }
-
 }
