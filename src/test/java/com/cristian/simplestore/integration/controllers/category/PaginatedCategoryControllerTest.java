31ee74cba26a501aa0ba6d3bd93d6fb247085eb5
bit5 <tec.cmos@gmail.com>
2019-05-08 14:27:20 -0500

    test the category pagination

--- src/test/java/com/cristian/simplestore/integration/controllers/category/PaginatedCategoryControllerTest.java
+++ src/test/java/com/cristian/simplestore/integration/controllers/category/PaginatedCategoryControllerTest.java
@@ -3,7 +3,7 @@ package com.cristian.simplestore.integration.controllers.category;
 import static org.assertj.core.api.Assertions.assertThat;
 import java.io.IOException;
 import java.util.List;
-import java.util.NoSuchElementException;
+import java.util.Map;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,12 +14,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import com.cristian.simplestore.integration.controllers.BaseIntegrationTest;
 import com.cristian.simplestore.integration.controllers.category.request.AuthenticatedCategoryRequest;
-import com.cristian.simplestore.persistence.entities.Category;
-import com.cristian.simplestore.persistence.repositories.CategoryRepository;
 import com.cristian.simplestore.utils.CategoryTestFactory;
-import com.cristian.simplestore.utils.MultiPartFormBuilder;
 import com.cristian.simplestore.utils.RequestBuilder;
-import com.cristian.simplestore.web.dto.response.CategoryResponse;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 
@@ -29,28 +25,34 @@ public class PaginatedCategoryControllerTest extends BaseIntegrationTest {
 
   @Autowired
   private CategoryTestFactory categoryFactory;
-  
+
   @Autowired
   private AuthenticatedCategoryRequest categoryRequest;
 
   @Test
   public void testItFindsAllCategories()
       throws JsonParseException, JsonMappingException, IOException {
-    long MAX_CATEGORIES_SIZE = 50;
-    List<Category> categories = categoryFactory.saveRandomCategoriesOnDb(MAX_CATEGORIES_SIZE);
+    long MAX_CATEGORIES_SIZE = 20;
+    categoryFactory.saveRandomCategoriesOnDb(MAX_CATEGORIES_SIZE);
+    Integer page = 0;
+    Integer size = 5;
 
-    ResponseEntity<String> response = categoryRequest.sendFindAllCategoriesRequest();
-
+    ResponseEntity<String> response = categoryRequest.buildFindAllCategoriesRequest()
+        .addRequestParam("page", String.valueOf(page)).addRequestParam("size", String.valueOf(size)).send();
+    
     List<?> responseCategories =
         (List<?>) RequestBuilder.getContentFromJsonRespose(response.getBody(), List.class);
 
-    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-    assertThat(responseCategories.size()).isEqualTo(categories.size());
-  }
+    Map<?,?> paginator = RequestBuilder.getPaginatorFromJsonRespose(response.getBody(), Map.class);
 
-  private void assertThatDtoisEqualToForm(CategoryResponse categoryDto, MultiPartFormBuilder form) {
-    assertThat(categoryDto.getName()).isEqualTo(form.get("name"));
-    assertThat(((Integer) categoryDto.getParentCategory().get("id")).longValue())
-        .isEqualTo(form.get("parentCategory"));
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    assertThat(responseCategories.size()).isEqualTo(size);
+    assertThat(paginator.get("currentPage")).isEqualTo(page);
+    assertThat(paginator.get("hasMorePages")).isEqualTo(true);
+    assertThat(paginator.get("nextPage")).isEqualTo(page + 1);
+    assertThat(paginator.get("hasPrevious")).isEqualTo(false);
+    assertThat(paginator.get("previousPage")).isEqualTo(null);
+    assertThat(paginator.get("pageSize")).isEqualTo(size);
+    assertThat(paginator.get("totalPages")).isEqualTo(((int) (MAX_CATEGORIES_SIZE / size)));   
   }
 }
