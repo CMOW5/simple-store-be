46668ea91b72cc3ea02cc0f8209fce370f2cfda6
bit5 <tec.cmos@gmail.com>
2019-05-17 23:01:22 -0500

    RequestBuilder replaced by RequestSender

--- src/test/java/com/cristian/simplestore/integration/controllers/category/request/UnauthenticatedCategoryRequest.java
+++ src/test/java/com/cristian/simplestore/integration/controllers/category/request/UnauthenticatedCategoryRequest.java
@@ -2,13 +2,13 @@ package com.cristian.simplestore.integration.controllers.category.request;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import com.cristian.simplestore.utils.RequestBuilder;
+import com.cristian.simplestore.utils.request.RequestSender;
 
 @Component
 public class UnauthenticatedCategoryRequest extends CategoryRequest {
 
   @Autowired
-  public UnauthenticatedCategoryRequest(RequestBuilder requestBuilder) {
-    super(requestBuilder);
+  public UnauthenticatedCategoryRequest(RequestSender requestSender) {
+    super(requestSender);
   }
 }
