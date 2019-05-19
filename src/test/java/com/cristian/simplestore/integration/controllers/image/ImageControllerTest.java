c8c94ce06c513cd8bf2841d47dd54c9cd6a4e0ff
CMOW5 <tec.cmos@gmail.com>
2019-05-18 20:49:51 -0500

    image requests methods moved to ImageRequest class

--- src/test/java/com/cristian/simplestore/integration/controllers/image/ImageControllerTest.java
+++ src/test/java/com/cristian/simplestore/integration/controllers/image/ImageControllerTest.java
@@ -1,4 +1,4 @@
-package com.cristian.simplestore.integration.controllers;
+package com.cristian.simplestore.integration.controllers.image;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import java.io.IOException;
@@ -8,14 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.core.io.Resource;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import com.cristian.simplestore.business.services.ImageService;
+import com.cristian.simplestore.integration.controllers.BaseIntegrationTest;
 import com.cristian.simplestore.persistence.entities.Image;
 import com.cristian.simplestore.utils.ImageFileFactory;
-import com.cristian.simplestore.utils.RequestBuilder;
+import com.cristian.simplestore.utils.request.JsonResponse;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -25,16 +24,16 @@ public class ImageControllerTest extends BaseIntegrationTest {
   private ImageService imageService;
 
   @Autowired
-  private ImageFileFactory imageBuilder;
+  private ImageFileFactory imageFileFactory;
   
   @Autowired
-  private RequestBuilder requestBuilder;
+  private ImageRequest imageRequest;
 
   @Test
   public void testItServesAnImage() throws IOException {
     Image image = saveRandomImageOnDb();
 
-    ResponseEntity<String> response = sendGetImageRequest(image.getName());
+    JsonResponse response = imageRequest.sendGetImageRequest(image.getName());
 
     assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
     assertThat(response.getBody()).isNotNull();
@@ -42,24 +41,18 @@ public class ImageControllerTest extends BaseIntegrationTest {
 
   @Test
   public void testItDoesNotFindAnImage() throws IOException {
-    String name = "some random name.jpg";
+    String name = "some_random_name.jpg";
 
-    ResponseEntity<String> response = sendGetImageRequest(name);
+    JsonResponse response = imageRequest.sendGetImageRequest(name);
 
     assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
   }
 
   private Image saveRandomImageOnDb() throws IOException {
-    Resource resource = imageBuilder.storeImageOnDisk();
+    Resource resource = imageFileFactory.storeImageOnDisk();
 
     Image image = new Image();
     image.setName(resource.getFile().getPath());
     return this.imageService.save(image);
-  }
-
-  public ResponseEntity<String> sendGetImageRequest(String imageName) {
-    String url = "/api/images/" + imageName;
-    ResponseEntity<String> response = requestBuilder.url(url).httpMethod(HttpMethod.GET).send();
-    return response;
   }
 }
