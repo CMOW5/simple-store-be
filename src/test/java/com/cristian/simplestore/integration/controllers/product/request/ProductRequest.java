3c96d6a3c4d42d037ec668dd462ea7c2771a5963
CMOW5 <tec.cmos@gmail.com>
2019-05-18 20:50:34 -0500

    the class now extends TestRequest

--- src/test/java/com/cristian/simplestore/integration/controllers/product/request/ProductRequest.java
+++ src/test/java/com/cristian/simplestore/integration/controllers/product/request/ProductRequest.java
@@ -6,81 +6,83 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
+
+import com.cristian.simplestore.integration.controllers.TestRequest;
 import com.cristian.simplestore.utils.MultiPartFormBuilder;
-import com.cristian.simplestore.utils.RequestBuilder;
+import com.cristian.simplestore.utils.request.JsonResponse;
+import com.cristian.simplestore.utils.request.RequestEntityBuilder;
+import com.cristian.simplestore.utils.request.RequestSender;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 
-public abstract class ProductRequest {
+public abstract class ProductRequest extends TestRequest {
 
-  private RequestBuilder requestBuilder;
+	public ProductRequest(RequestSender requestSender) {
+		super(requestSender);
+	}
 
-  public ProductRequest(RequestBuilder requestBuilder) {
-    this.requestBuilder = requestBuilder;
-  }
+	public JsonResponse sendFindAllProductsRequest() {
+		RequestEntityBuilder requestBuilder = createFindAllProductsRequest();
+		ResponseEntity<String> jsonResponse = send(requestBuilder.build());
+		return new JsonResponse(jsonResponse);
+	}
 
-  public ResponseEntity<String> sendFindAllProductsRequest() {
-    RequestBuilder request = buildFindAllProductsRequest();
-    ResponseEntity<String> response = request.send();
-    return response;
-  }
+	protected RequestEntityBuilder createFindAllProductsRequest() {
+		String url = "/api/admin/products";
+		return new RequestEntityBuilder().url(url).httpMethod(HttpMethod.GET);
+	}
 
-  protected RequestBuilder buildFindAllProductsRequest() {
-    String url = "/api/admin/products";
-    return requestBuilder.url(url).httpMethod(HttpMethod.GET);
-  }
+	public JsonResponse sendFindProductByIdRequest(Long id)
+			throws JsonParseException, JsonMappingException, IOException {
+		RequestEntityBuilder requestBuilder = createFindProductByIdRequest(id);
+		ResponseEntity<String> jsonResponse = send(requestBuilder.build());
+		return new JsonResponse(jsonResponse);
+	}
 
-  public ResponseEntity<String> sendFindProductByIdRequest(Long id)
-      throws JsonParseException, JsonMappingException, IOException {
-    RequestBuilder request = buildFindProductByIdRequest(id);
-    ResponseEntity<String> response = request.send();
-    return response;
-  }
+	protected RequestEntityBuilder createFindProductByIdRequest(Long id) {
+		String url = "/api/admin/products/" + id;
+		return new RequestEntityBuilder().url(url).httpMethod(HttpMethod.GET);
+	}
 
-  protected RequestBuilder buildFindProductByIdRequest(Long id) {
-    String url = "/api/admin/products/" + id;
-    return requestBuilder.url(url).httpMethod(HttpMethod.GET);
-  }
+	public JsonResponse sendProductCreateRequest(MultiPartFormBuilder form)
+			throws JsonParseException, JsonMappingException, IOException {
+		RequestEntityBuilder requestBuilder = createProductCreateRequest(form);
+		ResponseEntity<String> jsonResponse = send(requestBuilder.build());
+		return new JsonResponse(jsonResponse);
+	}
 
-  public ResponseEntity<String> sendProductCreateRequest(MultiPartFormBuilder form)
-      throws JsonParseException, JsonMappingException, IOException {
-    RequestBuilder request = buildProductCreateRequest(form);
-    ResponseEntity<String> response = request.send();
-    return response;
-  }
+	protected RequestEntityBuilder createProductCreateRequest(MultiPartFormBuilder form) {
+		String url = "/api/admin/products";
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+		MultiValueMap<String, Object> body = form.build();
+		return new RequestEntityBuilder().url(url).httpMethod(HttpMethod.POST).headers(headers).body(body);
+	}
 
-  protected RequestBuilder buildProductCreateRequest(MultiPartFormBuilder form) {
-    String url = "/api/admin/products";
-    HttpHeaders headers = new HttpHeaders();
-    headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-    MultiValueMap<String, Object> body = form.build();
-    return requestBuilder.url(url).httpMethod(HttpMethod.POST).headers(headers).body(body);
-  }
+	public JsonResponse sendProductUpdateRequest(Long productId, MultiPartFormBuilder form)
+			throws JsonParseException, JsonMappingException, IOException {
+		RequestEntityBuilder requestBuilder = createProductUpdateRequest(productId, form);
+		ResponseEntity<String> jsonResponse = send(requestBuilder.build());
+		return new JsonResponse(jsonResponse);
+	}
 
-  public ResponseEntity<String> sendProductUpdateRequest(Long productId, MultiPartFormBuilder form)
-      throws JsonParseException, JsonMappingException, IOException {
-    RequestBuilder request = buildProductUpdateRequest(productId, form);
-    ResponseEntity<String> response = request.send();
-    return response;
-  }
+	protected RequestEntityBuilder createProductUpdateRequest(Long productId, MultiPartFormBuilder form) {
+		String url = "/api/admin/products/" + productId;
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+		MultiValueMap<String, Object> body = form.build();
+		return new RequestEntityBuilder().url(url).httpMethod(HttpMethod.PUT).headers(headers).body(body);
+	}
 
-  protected RequestBuilder buildProductUpdateRequest(Long productId, MultiPartFormBuilder form) {
-    String url = "/api/admin/products/" + productId;
-    HttpHeaders headers = new HttpHeaders();
-    headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-    MultiValueMap<String, Object> body = form.build();
-    return requestBuilder.url(url).httpMethod(HttpMethod.PUT).headers(headers).body(body);
-  }
+	public JsonResponse sendProductDeleteRequest(Long productId)
+			throws JsonParseException, JsonMappingException, IOException {
+		RequestEntityBuilder requestBuilder = createProductDeleteRequest(productId);
+		ResponseEntity<String> jsonResponse = send(requestBuilder.build());
+		return new JsonResponse(jsonResponse);
+	}
 
-  public ResponseEntity<String> sendProductDeleteRequest(Long productId)
-      throws JsonParseException, JsonMappingException, IOException {
-    RequestBuilder request = buildProductDeleteRequest(productId);
-    ResponseEntity<String> response = request.send();
-    return response;
-  }
-  
-  protected RequestBuilder buildProductDeleteRequest(Long productId) {
-    String url = "/api/admin/products/" + productId;
-    return requestBuilder.url(url).httpMethod(HttpMethod.DELETE);
-  }
+	protected RequestEntityBuilder createProductDeleteRequest(Long productId) {
+		String url = "/api/admin/products/" + productId;
+		return new RequestEntityBuilder().url(url).httpMethod(HttpMethod.DELETE);
+	}
 }
