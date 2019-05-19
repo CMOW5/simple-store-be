09c5c8d2cb11369aa05624c1f9dbfcf6e75d63d6
CMOW5 <tec.cmos@gmail.com>
2019-05-18 20:47:06 -0500

    ResponseEntity Replaced by JsonResponse

--- src/test/java/com/cristian/simplestore/integration/controllers/product/AuthProductControllerTest.java
+++ src/test/java/com/cristian/simplestore/integration/controllers/product/AuthProductControllerTest.java
@@ -8,12 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import com.cristian.simplestore.integration.controllers.BaseIntegrationTest;
 import com.cristian.simplestore.integration.controllers.product.request.UnauthenticatedProductRequest;
 import com.cristian.simplestore.utils.MultiPartFormBuilder;
 import com.cristian.simplestore.utils.ProductTestsUtils;
+import com.cristian.simplestore.utils.request.JsonResponse;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 
@@ -21,58 +21,61 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class AuthProductControllerTest extends BaseIntegrationTest {
 
-  @Autowired
-  private ProductTestsUtils productsUtils;
-  
-  @Autowired
-  private UnauthenticatedProductRequest request;
-   
-  @Test
-  public void testItFailsFindsAllProductsWithoutCredentials()
-      throws JsonParseException, JsonMappingException, IOException {
-    ResponseEntity<String> response = request.sendFindAllProductsRequest();
-    assertResponseHasUnauthorizedStatus(response);
-  }
+	@Autowired
+	private ProductTestsUtils productsUtils;
 
-  @Test
-  public void testItFailsFindProductByIdWithoutCredentials()
-      throws JsonParseException, JsonMappingException, IOException {
-    Long productId = 1L;
-    
-    ResponseEntity<String> response = request.sendFindProductByIdRequest(productId);
-    
-    assertResponseHasUnauthorizedStatus(response);
-  }
+	@Autowired
+	private UnauthenticatedProductRequest request;
 
-  @Test
-  public void testItFailsCreateProductWithoutCredentials() throws JsonParseException, JsonMappingException, IOException {
-    MultiPartFormBuilder form = productsUtils.generateRandomProductCreateRequestForm();
-    
-    ResponseEntity<String> response = request.sendProductCreateRequest(form);
-    
-    assertResponseHasUnauthorizedStatus(response);
-  }
+	@Test
+	public void testItFailsFindsAllProductsWithoutCredentials()
+			throws JsonParseException, JsonMappingException, IOException {
+		JsonResponse response = request.sendFindAllProductsRequest();
+		assertResponseHasUnauthorizedStatus(response);
+	}
 
-  @Test
-  public void testItFailsUpdateProductWithoutCredentials() throws JsonParseException, JsonMappingException, IOException {
-    Long productId = 1L;
-    MultiPartFormBuilder form = productsUtils.generateRandomProductUpdateRequestForm();
+	@Test
+	public void testItFailsFindProductByIdWithoutCredentials()
+			throws JsonParseException, JsonMappingException, IOException {
+		Long productId = 1L;
 
-    ResponseEntity<String> response = request.sendProductUpdateRequest(productId, form);
+		JsonResponse response = request.sendFindProductByIdRequest(productId);
 
-    assertResponseHasUnauthorizedStatus(response);
-  }
+		assertResponseHasUnauthorizedStatus(response);
+	}
 
-  @Test
-  public void testItFailsDeletesProductWithoutCredentials() throws JsonParseException, JsonMappingException, IOException {
-    Long productId = 1L;
-    
-    ResponseEntity<String> response = request.sendProductDeleteRequest(productId);
-    
-    assertResponseHasUnauthorizedStatus(response);
-  }
-  
-  private void assertResponseHasUnauthorizedStatus(ResponseEntity<String> response) {
-    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
-  }
+	@Test
+	public void testItFailsCreateProductWithoutCredentials()
+			throws JsonParseException, JsonMappingException, IOException {
+		MultiPartFormBuilder form = productsUtils.generateRandomProductCreateRequestForm();
+
+		JsonResponse response = request.sendProductCreateRequest(form);
+
+		assertResponseHasUnauthorizedStatus(response);
+	}
+
+	@Test
+	public void testItFailsUpdateProductWithoutCredentials()
+			throws JsonParseException, JsonMappingException, IOException {
+		Long productId = 1L;
+		MultiPartFormBuilder form = productsUtils.generateRandomProductUpdateRequestForm();
+
+		JsonResponse response = request.sendProductUpdateRequest(productId, form);
+
+		assertResponseHasUnauthorizedStatus(response);
+	}
+
+	@Test
+	public void testItFailsDeletesProductWithoutCredentials()
+			throws JsonParseException, JsonMappingException, IOException {
+		Long productId = 1L;
+
+		JsonResponse response = request.sendProductDeleteRequest(productId);
+
+		assertResponseHasUnauthorizedStatus(response);
+	}
+
+	private void assertResponseHasUnauthorizedStatus(JsonResponse response) {
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
+	}
 }
