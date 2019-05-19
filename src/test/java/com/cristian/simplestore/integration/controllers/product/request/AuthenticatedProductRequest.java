1b6c122bfb923fb8b098c5522f9a028cda7a461c
CMOW5 <tec.cmos@gmail.com>
2019-05-18 20:46:21 -0500

    RequestBuilder raplaced by RequestEntityBuilder, Token generator added

--- src/test/java/com/cristian/simplestore/integration/controllers/product/request/AuthenticatedProductRequest.java
+++ src/test/java/com/cristian/simplestore/integration/controllers/product/request/AuthenticatedProductRequest.java
@@ -3,43 +3,52 @@ package com.cristian.simplestore.integration.controllers.product.request;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import com.cristian.simplestore.utils.MultiPartFormBuilder;
-import com.cristian.simplestore.utils.RequestBuilder;
+import com.cristian.simplestore.utils.request.RequestEntityBuilder;
+import com.cristian.simplestore.utils.request.RequestSender;
+import com.cristian.simplestore.utils.request.TokenGenerator;
 
 @Component
 public class AuthenticatedProductRequest extends ProductRequest {
-  
-  @Autowired
-  public AuthenticatedProductRequest(RequestBuilder requestBuilder) {
-    super(requestBuilder);
-  }
-  
-  @Override
-  protected RequestBuilder buildFindAllProductsRequest() {
-    RequestBuilder requestBuilder = super.buildFindAllProductsRequest();
-    return requestBuilder.withJwtAuth();
-  }
-  
-  @Override
-  protected RequestBuilder buildFindProductByIdRequest(Long id) {
-    RequestBuilder requestBuilder = super.buildFindProductByIdRequest(id);
-    return requestBuilder.withJwtAuth();
-  }
-  
-  @Override
-  protected RequestBuilder buildProductCreateRequest(MultiPartFormBuilder form) {
-    RequestBuilder requestBuilder = super.buildProductCreateRequest(form);
-    return requestBuilder.withJwtAuth();
-  }
-   
-  @Override
-  protected RequestBuilder buildProductUpdateRequest(Long categoryId, MultiPartFormBuilder form) {
-    RequestBuilder requestBuilder = super.buildProductUpdateRequest(categoryId, form);
-    return requestBuilder.withJwtAuth();
-  }
-  
-  @Override
-  protected RequestBuilder buildProductDeleteRequest(Long categoryId) {
-    RequestBuilder requestBuilder = super.buildProductDeleteRequest(categoryId);
-    return requestBuilder.withJwtAuth();
-  }
+
+	protected TokenGenerator tokenGenerator;
+	
+	@Autowired
+	public AuthenticatedProductRequest(RequestSender requestSender, TokenGenerator tokenGenerator) {
+		super(requestSender);
+		this.tokenGenerator = tokenGenerator;
+	}
+
+	@Override
+	protected RequestEntityBuilder createFindAllProductsRequest() {
+		RequestEntityBuilder requestBuilder = super.createFindAllProductsRequest();
+		return addJwtAuthToRequest(requestBuilder);
+	}
+
+	@Override
+	protected RequestEntityBuilder createFindProductByIdRequest(Long id) {
+		RequestEntityBuilder requestBuilder = super.createFindProductByIdRequest(id);
+		return addJwtAuthToRequest(requestBuilder);
+	}
+
+	@Override
+	protected RequestEntityBuilder createProductCreateRequest(MultiPartFormBuilder form) {
+		RequestEntityBuilder requestBuilder = super.createProductCreateRequest(form);
+		return addJwtAuthToRequest(requestBuilder);
+	}
+
+	@Override
+	protected RequestEntityBuilder createProductUpdateRequest(Long categoryId, MultiPartFormBuilder form) {
+		RequestEntityBuilder requestBuilder = super.createProductUpdateRequest(categoryId, form);
+		return addJwtAuthToRequest(requestBuilder);
+	}
+
+	@Override
+	protected RequestEntityBuilder createProductDeleteRequest(Long categoryId) {
+		RequestEntityBuilder requestBuilder = super.createProductDeleteRequest(categoryId);
+		return addJwtAuthToRequest(requestBuilder);
+	}
+	
+	private RequestEntityBuilder addJwtAuthToRequest(RequestEntityBuilder requestBuilder) {
+	    return requestBuilder.withJwtAuth(tokenGenerator);
+	 }
 }
