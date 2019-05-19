e8d3b6004f8849ba0981f3d96caeb57a6caf7041
CMOW5 <tec.cmos@gmail.com>
2019-05-18 20:51:24 -0500

    RequestBuilder replaced by RequestSender

--- src/test/java/com/cristian/simplestore/integration/controllers/product/request/UnauthenticatedProductRequest.java
+++ src/test/java/com/cristian/simplestore/integration/controllers/product/request/UnauthenticatedProductRequest.java
@@ -2,13 +2,13 @@ package com.cristian.simplestore.integration.controllers.product.request;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import com.cristian.simplestore.utils.RequestBuilder;
+import com.cristian.simplestore.utils.request.RequestSender;
 
 @Component
 public class UnauthenticatedProductRequest extends ProductRequest {
   
   @Autowired
-  public UnauthenticatedProductRequest(RequestBuilder requestBuilder) {
-    super(requestBuilder);
+  public UnauthenticatedProductRequest(RequestSender requestSender) {
+    super(requestSender);
   }
 }
