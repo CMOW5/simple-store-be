63288de61a68d70bd68ef31a63f23f125661f011
bit5 <tec.cmos@gmail.com>
2019-05-23 15:38:04 -0500

    indentation fixed

--- src/test/java/com/cristian/simplestore/integration/controllers/category/CategoryControllerTest.java
+++ src/test/java/com/cristian/simplestore/integration/controllers/category/CategoryControllerTest.java
@@ -3,6 +3,7 @@ package com.cristian.simplestore.integration.controllers.category;
 import static org.assertj.core.api.Assertions.assertThat;
 import java.io.IOException;
 import java.util.List;
+import java.util.NoSuchElementException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import com.cristian.simplestore.integration.controllers.BaseIntegrationTest;
 import com.cristian.simplestore.integration.controllers.category.request.AuthenticatedCategoryRequest;
 import com.cristian.simplestore.persistence.entities.Category;
+import com.cristian.simplestore.persistence.repositories.CategoryRepository;
 import com.cristian.simplestore.utils.MultiPartFormBuilder;
 import com.cristian.simplestore.utils.category.CategoryFormUtils;
 import com.cristian.simplestore.utils.category.CategoryGenerator;
@@ -25,102 +27,114 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class CategoryControllerTest extends BaseIntegrationTest {
 
-	@Autowired
-	private CategoryFormUtils categoryFormUtils;
-	
-	@Autowired
-	private CategoryGenerator categoryGenerator;
+    @Autowired
+    private CategoryRepository categoryRepository;
 
-	@Autowired
-	private AuthenticatedCategoryRequest categoryRequest;
+    @Autowired
+    private CategoryFormUtils categoryFormUtils;
+    
+    @Autowired
+    private CategoryGenerator categoryGenerator;
 
-	@Test
-	public void testItFindsAllCategories() throws JsonParseException, JsonMappingException, IOException {
-		int MAX_CATEGORIES_SIZE = 4;
-		List<Category> categories = categoryGenerator.saveRandomCategoriesOnDb(MAX_CATEGORIES_SIZE);
+    @Autowired
+    private AuthenticatedCategoryRequest categoryRequest;
 
-		JsonResponse response = categoryRequest.sendFindAllCategoriesRequest();
+    @Test
+    public void testItFindsAllCategories() throws JsonParseException, JsonMappingException, IOException {
+        int MAX_CATEGORIES_SIZE = 4;
+        List<Category> categories = categoryGenerator.saveRandomCategoriesOnDb(MAX_CATEGORIES_SIZE);
 
-		List<?> responseCategories = (List<?>) response.getContentFromJsonRespose(List.class);
+        JsonResponse response = categoryRequest.sendFindAllCategoriesRequest();
 
-		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-		assertThat(responseCategories.size()).isEqualTo(categories.size());
-	}
+        List<?> responseCategories = (List<?>) response.getContentFromJsonRespose(List.class);
 
-	@Test
-	public void testItFindsACategoryById() throws JsonParseException, JsonMappingException, IOException {
-		Category category = categoryGenerator.saveRandomCategoryOnDb();
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseCategories.size()).isEqualTo(categories.size());
+    }
 
-		JsonResponse response = categoryRequest.sendFindCategoryByIdRequest(category.getId());
+    @Test
+    public void testItFindsACategoryById() throws JsonParseException, JsonMappingException, IOException {
+        Category category = categoryGenerator.saveRandomCategoryOnDb();
 
-		CategoryResponse foundCategory = (CategoryResponse) response.getContentFromJsonRespose(CategoryResponse.class);
+        JsonResponse response = categoryRequest.sendFindCategoryByIdRequest(category.getId());
 
-		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-		assertThat(foundCategory.getName()).isEqualTo(category.getName());
-		assertThat(foundCategory.getId()).isEqualTo(category.getId());
-	}
+        CategoryResponse foundCategory = (CategoryResponse) response.getContentFromJsonRespose(CategoryResponse.class);
 
-	@Test
-	public void testItDoesNotFindACategoryById() throws JsonParseException, JsonMappingException, IOException {
-		Long nonExistentCategoryId = 1L;
-		JsonResponse response = categoryRequest.sendFindCategoryByIdRequest(nonExistentCategoryId);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(foundCategory.getName()).isEqualTo(category.getName());
+        assertThat(foundCategory.getId()).isEqualTo(category.getId());
+    }
 
-		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
-	}
+    @Test
+    public void testItDoesNotFindACategoryById() throws JsonParseException, JsonMappingException, IOException {
+        Long nonExistentCategoryId = 1L;
+        JsonResponse response = categoryRequest.sendFindCategoryByIdRequest(nonExistentCategoryId);
 
-	@Test
-	public void testItCreatesACategory() throws JsonParseException, JsonMappingException, IOException {
-		MultiPartFormBuilder form = categoryFormUtils.generateRandomCategoryCreateRequestForm();
-		
-		JsonResponse response = categoryRequest.sendCategoryCreateRequest(form);
-		CategoryResponse createdCategory = (CategoryResponse) response
-				.getContentFromJsonRespose(CategoryResponse.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+    }
 
-		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
-		assertThatDtoisEqualToForm(createdCategory, form);
-		assertThat(createdCategory.getImage()).isNotNull();
-	}
+    @Test
+    public void testItCreatesACategory() throws JsonParseException, JsonMappingException, IOException {
+        MultiPartFormBuilder form = categoryFormUtils.generateRandomCategoryCreateRequestForm();
+        
+        JsonResponse response = categoryRequest.sendCategoryCreateRequest(form);
+        CategoryResponse createdCategory = (CategoryResponse) response
+                .getContentFromJsonRespose(CategoryResponse.class);
 
-	@Test
-	public void testItUpdatesACategory() throws JsonParseException, JsonMappingException, IOException {
-		Category categoryToUpdate = categoryGenerator.saveRandomCategoryOnDb();		
-		MultiPartFormBuilder form = categoryFormUtils.generateRandomCategoryUpdateRequestForm(); 
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThatDtoisEqualToForm(createdCategory, form);
+        assertThat(createdCategory.getImage()).isNotNull();
+    }
 
-		JsonResponse response = categoryRequest.sendCategoryUpdateRequest(categoryToUpdate.getId(), form);
-		CategoryResponse updatedCategory = (CategoryResponse) response
-				.getContentFromJsonRespose(CategoryResponse.class);
+    @Test
+    public void testItUpdatesACategory() throws JsonParseException, JsonMappingException, IOException {
+        Category categoryToUpdate = categoryGenerator.saveRandomCategoryOnDb();     
+        MultiPartFormBuilder form = categoryFormUtils.generateRandomCategoryUpdateRequestForm();
 
-		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-		assertThatDtoisEqualToForm(updatedCategory, form);
-		assertThat(updatedCategory.getImage().getId()).isNotEqualTo(categoryToUpdate.getImage().getId());
-	}
+        JsonResponse response = categoryRequest.sendCategoryUpdateRequest(categoryToUpdate.getId(), form);
+        CategoryResponse updatedCategory = (CategoryResponse) response
+                .getContentFromJsonRespose(CategoryResponse.class);
 
-	@Test
-	public void testItReturnsNotFoundWhenUpdating() throws JsonParseException, JsonMappingException, IOException {
-		Long nonExistentCategoryId = 1L;
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThatDtoisEqualToForm(updatedCategory, form);
+        assertThat(updatedCategory.getImage().getId()).isNotEqualTo(categoryToUpdate.getImage().getId());
+    }
 
-		MultiPartFormBuilder form = new MultiPartFormBuilder();
-		form.add("name", "some name");
+    @Test
+    public void testItReturnsNotFoundWhenUpdating() throws JsonParseException, JsonMappingException, IOException {
+        Long nonExistentCategoryId = 1L;
 
-		JsonResponse response = categoryRequest.sendCategoryUpdateRequest(nonExistentCategoryId, form);
+        MultiPartFormBuilder form = new MultiPartFormBuilder();
+        form.add("name", "some name");
 
-		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
-	}
+        JsonResponse response = categoryRequest.sendCategoryUpdateRequest(nonExistentCategoryId, form);
 
-	@Test
-	public void testItDeletesACategory() throws JsonParseException, JsonMappingException, IOException {
-		Category category = categoryGenerator.saveRandomCategoryOnDb();
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
 
-		JsonResponse response = categoryRequest.sendCategoryDeleteRequest(category.getId());
-		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
-		
-		response = categoryRequest.sendFindCategoryByIdRequest(category.getId());
-		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
-	}
+    @Test(expected = NoSuchElementException.class)
+    public void testItDeletesACategory() throws JsonParseException, JsonMappingException, IOException {
+        Category category = categoryGenerator.saveRandomCategoryOnDb();
 
-	private void assertThatDtoisEqualToForm(CategoryResponse categoryDto, MultiPartFormBuilder form) {
-		assertThat(categoryDto.getName()).isEqualTo(form.get("name"));
-		assertThat(((Integer) categoryDto.getParentCategory().get("id")).longValue())
-				.isEqualTo(form.get("parentCategory"));
-	}
+        JsonResponse response = categoryRequest.sendCategoryDeleteRequest(category.getId());
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+        // TODO: violation of test boundary
+        categoryRepository.findById(category.getId()).get();
+    }
+
+    @Test
+    public void testItReturnsNotFoundWhenDeleting() throws JsonParseException, JsonMappingException, IOException {
+        Long nonExistentId = 1L;
+
+        JsonResponse response = categoryRequest.sendCategoryDeleteRequest(nonExistentId);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+    }
+
+    private void assertThatDtoisEqualToForm(CategoryResponse categoryDto, MultiPartFormBuilder form) {
+        assertThat(categoryDto.getName()).isEqualTo(form.get("name"));
+        assertThat(((Integer) categoryDto.getParentCategory().get("id")).longValue())
+                .isEqualTo(form.get("parentCategory"));
+    }
 }
