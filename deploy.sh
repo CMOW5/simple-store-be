f39e21eb0fd7c7f2789cc009964adbde2f03fb02
CMOW5 <tec.cmos@gmail.com>
2019-05-26 22:20:56 -0500

    deploy script added (unfinished)

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,17 @@ gradle build
 git add build/libs/
 git commit -m "0.0.1 simple store"
 git subtree push --prefix build/libs production master
+
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+git checkout build
+git merge master
+gradle build 
+git add build/libs/
+git commit -m "0.0.1 simple store"
+git subtree push --prefix build/libs production master
+git checkout master
