f2bdb91fd274f78d5fb03872924b0720147306c8
CMOW5 <tec.cmos@gmail.com>
2019-06-01 18:10:02 -0500

    deploy script modified

--- deploy.sh
+++ deploy.sh
@@ -18,3 +18,5 @@ git add build/libs/
 git commit -m "0.0.1 simple store"
 git subtree push --prefix build/libs production master
 git checkout master
+
+nohup java -jar -Dspring.profiles.active=production simple-store-be-0.0.1-SNAPSHOT.jar &
