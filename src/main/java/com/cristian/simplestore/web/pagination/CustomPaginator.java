9561f0f9085663270dc6330e112349db6cc24997
CMOW5 <tec.cmos@gmail.com>
2019-06-09 18:14:38 -0500

    fomr

--- src/main/java/com/cristian/simplestore/web/pagination/CustomPaginator.java
+++ src/main/java/com/cristian/simplestore/web/pagination/CustomPaginator.java
@@ -1,5 +1,8 @@
 package com.cristian.simplestore.web.pagination;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface CustomPaginator extends Pageable {
@@ -21,4 +24,8 @@ public interface CustomPaginator extends Pageable {
   int getPageCount();
   
   String getPath();  
+  
+  public static <T> CustomPaginator of(Page<T> paginatedResult, int page, int size, HttpServletRequest request) {
+	  return new CustomPaginatorImpl(paginatedResult, page, size, request);
+  }
 }
