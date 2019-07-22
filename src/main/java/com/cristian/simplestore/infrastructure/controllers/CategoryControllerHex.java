dd2277183c742be1bcce0e847ef2870a5074befd
CMOW5 <tec.cmos@gmail.com>
2019-07-21 21:25:41 -0500

    category controller

--- src/main/java/com/cristian/simplestore/infrastructure/controllers/CategoryControllerHex.java
+++ src/main/java/com/cristian/simplestore/infrastructure/controllers/CategoryControllerHex.java
@@ -1,8 +1,10 @@
 package com.cristian.simplestore.infrastructure.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import com.cristian.simplestore.application.command.CreateCategoryCommand;
 import com.cristian.simplestore.application.handler.category.CreateCategoryHandler;
@@ -20,6 +22,7 @@ public class CategoryControllerHex {
   }
 
   @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
   public String create(CreateCategoryCommand command) {
     Category createdCategory = createCategoryHandler.execute(command);
     return "created category " + createdCategory.getName(); 
